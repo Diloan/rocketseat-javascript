f325f03c3aafea60081ba0c186bdf7611502368a
Diloan Silva <diloan.silva@gmail.com>
2020-02-18 23:28:24 -0300

    Novas aulas e ajustes

--- 03 - App-de-todos/todos.js
+++ 03 - App-de-todos/todos.js
@@ -3,20 +3,50 @@ var inputElement =  document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 
-var todos = [
-    'Fazer café',
-    'Estudar javascript',
-    'Acessar a comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos(){
+    listElemet.innerHTML = '';
+
     for(todo of todos){
         var todoElement = document.createElement('li');
         var todoText  = document.createTextNode(todo);
 
+        linkElement = document.createElement('a');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        linkText = document.createTextNode('Excluir');
+        linkElement.setAttribute('href', '#');
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElemet.appendChild(todoElement);
+        
     }
 }
 
 renderTodos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+    
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+    savaToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+    savaToStorage();
+}
+
+function savaToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
